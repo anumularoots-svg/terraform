d37b54c9d1415dd16b28bc15e8171abf0dddf975
anumularoots-svg <anumula.roots@gmail.com>
2025-11-18 13:54:03 +0530

    Update outputs.tf

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,19 +1,14 @@
 output "eks_cluster_role_arn" {
-  description = "EKS cluster IAM role ARN"
+  description = "ARN of the EKS cluster IAM role"
   value       = aws_iam_role.eks_cluster.arn
 }
 
 output "eks_node_role_arn" {
-  description = "EKS node IAM role ARN"
-  value       = aws_iam_role.eks_nodes.arn
-}
-
-output "jenkins_role_arn" {
-  description = "Jenkins IAM role ARN"
-  value       = aws_iam_role.jenkins.arn
+  description = "ARN of the EKS node IAM role"
+  value       = aws_iam_role.eks_node.arn
 }
 
 output "jenkins_instance_profile_name" {
-  description = "Jenkins instance profile name"
+  description = "Jenkins EC2 instance profile name"
   value       = aws_iam_instance_profile.jenkins.name
 }
